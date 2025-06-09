5479edd67d4b74911554f940c4364f1fba96ef8d
Marcelo Arias <hello@marceloarias.com>
2025-06-08 20:33:58 -0500

    Fix legacy Node problem on Netlify

--- site/utils/compile-webpack.js
+++ site/utils/compile-webpack.js
@@ -1,3 +1,8 @@
+// Polyfill for Node.js 17+ compatibility with webpack 4
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = (algorithm) => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
 const webpack = require("webpack");
 const TerserPlugin = require("terser-webpack-plugin");
 const MemoryFileSystem = require("memory-fs");
